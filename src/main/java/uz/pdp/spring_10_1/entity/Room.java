2549cadcc6182a97c9183e20a1d78e4afe7d9734
AladdinRukhiddinov <aladinrukhiddinov@gmail.com>
2022-03-07 13:03:57 +0500

    create Room.class

---
+++ src/main/java/uz/pdp/spring_10_1/entity/Room.java
@@ -0,0 +1,29 @@
+package uz.pdp.spring_10_1.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity(name = "rooms")
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private Integer number;
+
+    @Column(nullable = false)
+    private Integer floor;
+
+    @Column(nullable = false)
+    private String size;
+
+    @ManyToOne
+    private Hotel hotel;
+}
